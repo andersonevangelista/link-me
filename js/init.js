4262571945dea85e460808a6fdfca251ac2a3ad4
Anderson Evangelista <andersonevangelista1@gmail.com>
2020-01-22 22:04:26 -0300

    Conectando banco de dados Firebase ao projeto

---
+++ js/init.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyDRTUYsNb-UQYPYyS6KiYFGqcywmBLYlWw",
+    authDomain: "linkme-409ac.firebaseapp.com",
+    databaseURL: "https://linkme-409ac.firebaseio.com",
+    projectId: "linkme-409ac",
+    storageBucket: "linkme-409ac.appspot.com",
+    messagingSenderId: "391447148675",
+    appId: "1:391447148675:web:2b95aa21879e8d592c83b6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
